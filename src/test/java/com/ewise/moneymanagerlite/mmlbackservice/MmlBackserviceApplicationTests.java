0a0c12d4e284b40ff3bfdd563b6e210bae0028cf
Richard Mendoza <ewisedev@Richards-MacBook-Pro.local>
2018-12-05 17:26:22 +0800

    :tada: Initial Commit

---
+++ src/test/java/com/ewise/moneymanagerlite/mmlbackservice/MmlBackserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ewise.moneymanagerlite.mmlbackservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MmlBackserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
